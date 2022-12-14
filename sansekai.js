de7321afd768ad46011148dbe3d6aa64e0656907
orang bingung <113142200+veexor@users.noreply.github.com>
2022-12-15 00:13:50 +0700

    Update sansekai.js

--- sansekai.js
+++ sansekai.js
@@ -65,7 +65,7 @@ module.exports = sansekai = async (client, m, chatUpdate, store) => {
         if (budy) {
             try {
             // Jika apikey limit silahkan ganti apikey openainya di bawah ini 
-            const keyopenai = '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'
+            const keyopenai = 'sk-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'
             const configuration = new Configuration({
               apiKey: keyopenai, 
             });
