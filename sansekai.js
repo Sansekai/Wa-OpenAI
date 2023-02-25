02d744c57ada11a1783ba586f7f6c8a65ef83c95
M Yusril <34540627+Sansekai@users.noreply.github.com>
2023-02-26 00:04:30 +0800

    Update sansekai.js

--- sansekai.js
+++ sansekai.js
@@ -94,12 +94,12 @@ Membuat gambar dari teks`)
             const response = await openai.createCompletion({
               model: "text-davinci-003",
               prompt: text,
-              temperature: 0.3,
-              max_tokens: 2000,
-              top_p: 1.0,
-              frequency_penalty: 0.0,
-              presence_penalty: 0.0,
-            });
+              temperature: 0, // Higher values means the model will take more risks.
+              max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+              top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+              frequency_penalty: 0.3, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+              presence_penalty: 0 // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+          });;
             m.reply(`${response.data.choices[0].text}`);
           } catch (error) {
           if (error.response) {
