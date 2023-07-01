5981d53a5b1c809ac117c6df9db3fd96bdb84b8b
rii34 <114324093+rii34@users.noreply.github.com>
2023-07-02 04:27:56 +0700

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const sessionName = "yusril";
-const donet = "https://saweria.co/sansekai";
-const owner = ["6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
+const sessionName = "RiiDev";
+const donet = "-";
+const owner = ["6285817145056"]; // Put your number here ex: ["62xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
